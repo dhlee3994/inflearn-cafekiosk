0662b8c80b2dfecd0ebb12bea5be7c22efbb184a
dhlee3994 <dhlee3994@gmail.com>
2024-04-29 17:23:14 +0900

    @Mock, @InjectMocks 실습

---
+++ src/test/java/dev/dhlee/cafekiosk/spring/api/service/mail/MailServiceTest.java
@@ -0,0 +1,53 @@
+package dev.dhlee.cafekiosk.spring.api.service.mail;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import dev.dhlee.cafekiosk.spring.client.MailSendClient;
+import dev.dhlee.cafekiosk.spring.domain.history.MailSendHistory;
+import dev.dhlee.cafekiosk.spring.domain.history.MailSendHistoryRepository;
+
+@ExtendWith(MockitoExtension.class)
+class MailServiceTest {
+
+	@Mock
+	private MailSendClient mailSendClient;
+
+	@Mock
+	private MailSendHistoryRepository mailSendHistoryRepository;
+
+	@InjectMocks
+	private MailService mailService;
+
+	@DisplayName("메일 전송 테스트")
+	@Test
+	void sendMail() {
+		// given
+		//MailSendClient mailSendClient = mock(MailSendClient.class);
+		//MailSendHistoryRepository mailSendHistoryRepository = mock(MailSendHistoryRepository.class);
+
+		//MailService mailService = new MailService(mailSendClient, mailSendHistoryRepository);
+
+		// stubbing
+		when(mailSendClient.sendEmail(anyString(), anyString(), anyString(), anyString()))
+			.thenReturn(true);
+
+		// when
+		boolean result = mailService.sendMail("", "", "", "");
+
+		// then
+		assertThat(result).isTrue();
+		verify(mailSendHistoryRepository, times(1)).save(any(MailSendHistory.class));
+	}
+}
